d17fb22d9499c1ee7eb09e4233360bc2dc03028c
Gus Ruben <gusruben@protonmail.com>
2025-07-11 21:43:01 -0400

    Initial Commit

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,2 +1,80 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+	:global(body) {
+		background-color: #CCF4FD;
+	}
+</style>
+
+<div class="absolute top-0 left-0 w-full h-full bg-[url('brushstroking.png')] bg-repeat mix-blend-overlay opacity-30 pointer-events-none"></div>
+
+<div class="flex flex-col items-center justify-center h-screen text-center bg-gradient-to-b from-[#CCF4FD] to-[#9DACF2] bg-blend-overlay">
+
+	<div class="absolute top-0 left-0 w-full h-full bg-[url(/buildings-top.png)] bg-no-repeat bg-contain pointer-events-none -translate-y-15"></div>
+	<div class="inline-block relative">
+		<h2
+			class="text-xl font-serif bg-gradient-to-b from-[#487DAB] to-[#3F709A] bg-clip-text text-transparent absolute left-[calc(50%+3rem)] -translate-x-1/2 bottom-8 italic"
+		>
+			September 24th & 25th
+		</h2>
+		<img src="daydream.png" alt="Daydream" class="h-40 mb-6" />
+	</div>
+	<div class="relative inline-block">
+		<h3
+			class="text-3xl italic font-serif bg-gradient-to-b from-[#487DAB] to-[#3F709A] bg-clip-text text-transparent"
+		>
+			The World's Biggest High School Game Jam
+		</h3>
+		<img
+			src="underline.svg"
+			alt=""
+			class="absolute left-1/2 -translate-x-1/2 -mt-1 h-auto scale-115"
+		/>
+	</div>
+	
+	<div class="mt-8 flex flex-col items-center gap-3 z-10">
+		<input
+			type="email"
+			placeholder="Enter Your Email Address"
+			class="w-80 px-4 py-2 bg-white text-black rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-daydream font-sans"
+		/>
+		<button
+			class="w-80 px-4 py-2 bg-daydream text-white rounded-full border border-gray-300 hover:bg-daydream-hover transition-colors font-sans font-bold cursor-pointer"
+		>
+			SIGN UP NOW
+		</button>
+	</div>
+
+	<img src="hot-air-balloon.png" alt="" class="absolute w-1/8 right-32 bottom-40 z-20">
+	<img src="hot-air-balloon.png" alt="" class="absolute w-1/12 left-36 bottom-81 z-20">
+
+	<img src="/clouds-right.png" alt="" class="absolute right-0 w-7/12 -bottom-6">
+	<img src="/clouds-left.png" alt="" class="absolute left-0 w-5/12 -bottom-4">
+</div>
+
+<div class="flex flex-row flex-wrap w-full h-auto bg-gradient-to-b from-[#9DACF2] to-[#FDC5D1] px-36 pb-30">
+	<div class="flex flex-col items-center w-max basis-1/2">
+		<div class="bg-[#FFF5FA] border-2 border-[#AA8B83] rounded-2xl text-xl font-serif p-6 w-56 text-center translate-y-8">
+			Sign up for a Daydream event in your city
+		</div>
+		<img src="/island-1.png" alt="" class="w-72 h-72 object-contain">
+	</div>
+	<div class="flex flex-col items-center w-max basis-1/2 translate-y-24">
+		<div class="bg-[#FFF5FA] border-2 border-[#AA8B83] rounded-2xl text-xl font-serif p-6 w-56 text-center translate-y-24">
+			Find a team of other teenagers at Daydream
+		</div>
+		<img src="/island-3.png" alt="" class="w-86 h-86 object-contain">
+	</div>
+	<div class="flex flex-col items-center w-max basis-1/2 -translate-x-12">
+		<div class="bg-[#FFF5FA] border-2 border-[#AA8B83] rounded-2xl text-xl font-serif p-6 w-56 text-center translate-y-8">
+			Start building your game - <u>No experience needed</u>
+		</div>
+		<img src="/island-2.png" alt="" class="w-72 h-72 object-contain">
+	</div>
+		<div class="flex flex-col items-center w-max basis-1/2 translate-y-30">
+		<div class="bg-[#FFF5FA] border-2 border-[#AA8B83] rounded-2xl text-xl font-serif p-6 w-56 text-center translate-y-24">
+			Attend workshops or talk to one of our mentors for help
+		</div>
+		<img src="/island-4.png" alt="" class="w-88 h-88 object-contain">
+	</div>
+</div>
+
+<div class="absolute top-0 left-0 w-full h-full bg-[url('brushstroking.png')] bg-repeat mix-blend-overlay opacity-30 pointer-events-none"></div>
