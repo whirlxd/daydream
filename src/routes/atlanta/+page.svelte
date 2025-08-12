ce2b943b01885651a79ba3d753dc4cdaaf84620e
Gus Ruben <gusruben@protonmail.com>
2025-08-12 18:49:51 -0400

    Remove TBD from atlanta site

--- src/routes/atlanta/+page.svelte
+++ src/routes/atlanta/+page.svelte
@@ -9,9 +9,9 @@
 	// Configuration - Put your information here!
 	const eventName = "Atlanta Hackathon/Game Jam";
 	const eventLocation = "Atlanta";
-	const eventAddress = "TBD"; // Leave this empty if you don't want an address
+	const eventAddress = ""; // Leave this empty if you don't want an address
 	// These two are optional
-	const directionsURL = "TBD"
+	const directionsURL = ""
 	const contactLink = "loganpeterson@hackclub.app"
 	
 	// Sponsors Configuration
