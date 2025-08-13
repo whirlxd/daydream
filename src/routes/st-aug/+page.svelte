9ec015503d50ea02e997c765270051ed4229d66a
Gus Ruben <gusruben@protonmail.com>
2025-08-13 16:08:05 -0400

    Rename St. Aug to St. Augustine

--- src/routes/st-aug/+page.svelte
+++ src/routes/st-aug/+page.svelte
@@ -7,7 +7,7 @@
 	 */
 
 	// Configuration - Put your information here!
-	const eventName = "St. Aug";
+	const eventName = "St. Augustine";
 	const eventLocation = "Saint Augustine, FL";
 	const eventAddress = ""; // Leave this empty if you don't want an address
 	// These two are optional
