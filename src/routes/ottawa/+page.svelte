164728a48aeb6c82b379e1921588367927b00afb
Kashyap Sukshavasi <101945096+kashsuks@users.noreply.github.com>
2025-08-13 17:55:35 -0400

    remove location for ottawa (TBD)

--- src/routes/ottawa/+page.svelte
+++ src/routes/ottawa/+page.svelte
@@ -9,9 +9,9 @@
 	// Configuration - Put your information here!
 	const eventName = "Ottawa"; // The name of your event
 	const eventLocation = "Ottawa";
-	const eventAddress = "4000 Innovation DrKanata, ON K2K 3K1"; // Leave this empty if you don't want an address
+	const eventAddress = "TBD"; // Leave this empty if you don't want an address
 	// These two are optional
-	const directionsURL = "https://www.google.com/maps/search/4000+Innovation+DrKanata,+ON+K2K+3K1"
+	const directionsURL = "https://daydream.hackclub.com/ottawa"
 	const contactLink = "mailto:ottawa@daydream.hackclub.com"
 
 	// Sponsors Configuration
