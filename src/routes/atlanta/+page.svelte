51d268623b64d83ad401eb338bbe2debb2982c1f
ayush jaiswal <ayushjaiswal18.2009@gmail.com>
2025-08-16 15:49:48 -0400

    fixed

--- src/routes/atlanta/+page.svelte
+++ src/routes/atlanta/+page.svelte
@@ -7,7 +7,7 @@
 	 */
 
 	// Configuration - Put your information here!
-	const eventName = "Atlanta";
+	const eventName = "Daydream Atlanta";
 	const eventLocation = "Atlanta";
 	const eventAddress = ""; // Leave this empty if you don't want an address
 	// These two are optional
@@ -18,7 +18,7 @@
 	const signupLink = "https://forms.hackclub.com/daydream-rsvp"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
 	const sponsorsEnabled = false; // false for now...
 	const sponsors = [
-		{ image: "/example/logo1.png", name: "Sponsor 1", url: "https://example1.com" },
+		{ image: "jukebox.png", name: "Jukebox Prints!", url: "https://www.jukeboxprint.com/custom-stickers" },
 		{ image: "/example/logo2.png", name: "Sponsor 2", url: "https://example2.com" },
 		{ image: "/example/logo3.png", name: "Sponsor 3", url: "https://example3.com" },
 		{ image: "/example/logo4.png", name: "Sponsor 4", url: "https://example4.com" },
@@ -819,7 +819,7 @@ Mumbai`.split("\n")
 			>
 				September 27th & 28th, 2025
 			</h2>
-			<img src="daydream.png" alt="Daydream" class="h-40 mb-6 w-auto object-contain max-w-full px-4" />
+			<img src="daydreamatl.png" alt="Daydream" class="h-40 mb-6 w-auto object-contain max-w-full px-4" />
 			<a href="https://hackclub.com" class="absolute top-0 -right-6 max-sm:right-0 max-sm:scale-80 animate-hover ![animation-delay:0.9s] ![--hover:-0.2rem]">
 				<img src="flag-plane.png" alt="Hack Club" class="h-28">
 			</a>
