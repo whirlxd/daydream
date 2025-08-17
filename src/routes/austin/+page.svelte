4d514c4ef8d5a03f736eede0e26c318c76e72b4c
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2025-08-17 18:09:17 +0000

    add rough schedule for austin

--- src/routes/austin/+page.svelte
+++ src/routes/austin/+page.svelte
@@ -19,12 +19,12 @@
 	const sponsorsEnabled = false; // Set to false to hide the entire sponsors section
 	const sponsors = [
 		{ image: "/example/logo1.png", name: "Sponsor 1", url: "https://example1.com" },
-		{ image: "/example/logo2.png", name: "Sponsor 2", url: "https://example2.com" },
-		{ image: "/example/logo3.png", name: "Sponsor 3", url: "https://example3.com" },
-		{ image: "/example/logo4.png", name: "Sponsor 4", url: "https://example4.com" },
-		{ image: "/example/logo5.png", name: "Sponsor 5", url: "https://example5.com" },
-		{ image: "/example/logo6.png", name: "Sponsor 6", url: "https://example6.com" },
-		{ image: "/example/logo7.png", name: "Sponsor 7", url: "https://example7.com" }
+		// { image: "/example/logo2.png", name: "Sponsor 2", url: "https://example2.com" },
+		// { image: "/example/logo3.png", name: "Sponsor 3", url: "https://example3.com" },
+		// { image: "/example/logo4.png", name: "Sponsor 4", url: "https://example4.com" },
+		// { image: "/example/logo5.png", name: "Sponsor 5", url: "https://example5.com" },
+		// { image: "/example/logo6.png", name: "Sponsor 6", url: "https://example6.com" },
+		// { image: "/example/logo7.png", name: "Sponsor 7", url: "https://example7.com" }
 	];
 	
 	// Schedule Configuration - You don't need to use this exact schedule, this is just an example!
@@ -32,26 +32,23 @@
 		{
 			title: "Saturday, September 27th",
 			items: [
-				{ event: "Coming Soon!", time: ""}
-				// { event: "Doors open", time: "11:00 AM" },
-				// { event: "Opening ceremony", time: "12:00 PM" },
-				// { event: "Lunch", time: "12:30 PM" },
-				// { event: "Start working on your project!", time: "1:00 PM" },
-				// { event: "Workshop 1", time: "2:00 PM" },
-				// { event: "Activity 1", time: "4:00 PM" },
-				// { event: "Workshop 2", time: "4:00 PM" },
-				// { event: "Dinner", time: "6:00 PM" },
-				// { event: "Lightning talks", time: "8:00 PM" },
-				// { event: "Midnight surprise", time: "12:00 AM" }
+				{ event: "Doors open", time: "11:00 AM" },
+				{ event: "Opening ceremony", time: "12:00 PM" },
+				{ event: "Lunch", time: "12:30 PM" },
+				{ event: "Workshop 1", time: "1:30 PM" },
+				{ event: "Workshop 2", time: "2:00 PM" },
+				// { event: "Workshop 3", time: "4:00 PM" },
+				{ event: "Dinner and Lightning Talks", time: "6:00 PM" },
+				// { event: "Midnight surprise (ice cream?)", time: "12:00 AM" }
 			]
 		},
 		{
 			title: "Sunday, September 28th",
 			items: [
-				{ event: "Coming Soon!", time: "" }
-				// { event: "Breakfast", time: "8:00 AM" },
-				// { event: "Demos!", time: "10:30 AM" },
-				// { event: "Closing ceremony", time: "12:00 PM" }
+				{ event: "Breakfast", time: "8:30 AM" },
+				{ event: "Submissions Open", time: "11:00 AM" },
+				{ event: "Submissions Close and Judging Starts", time: "11:30 AM" },
+				{ event: "Closing ceremony", time: "12:30 PM" }
 			]
 		}
 	];
