f66d4d6f73fec62f59fcfbaae8e27a6f07bb8cc0
ItsMeNoobieboy <54125080+ItsMeNoobieboy@users.noreply.github.com>
2025-08-19 17:01:24 -0500

    Update Missouri sign up link

--- src/routes/missouri/+page.svelte
+++ src/routes/missouri/+page.svelte
@@ -10,7 +10,7 @@
 	const eventName = "Missouri"; // This should be the name of your event WITHOUT "Daydream" at the start
 	const eventLocation = "Springfield, Missouri";
 	const eventAddress = "The Library Center, 4653 S Campbell Ave, Springfield, MO 65810"; // Leave this empty if you don't want an address
-	const signupLink = "https://forms.hackclub.com/daydream-rsvp?event=recukCDrvwSE2bU26"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
+	const signupLink = "https://forms.hackclub.com/daydream-sign-up?event=recukCDrvwSE2bU26"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
 	// These two are optional-- leave them empty if you don't have anything!
 	const directionsURL = ""
 	const contactLink = "mailto:missouri@daydream.hackclub.com"
