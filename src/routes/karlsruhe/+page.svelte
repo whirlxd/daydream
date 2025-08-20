6ece837f705919169243b9b4fc91891f999ca0e6
ItsMeNoobieboy <54125080+ItsMeNoobieboy@users.noreply.github.com>
2025-08-19 19:29:29 -0500

    Update signup link Karlsruhe

--- src/routes/karlsruhe/+page.svelte
+++ src/routes/karlsruhe/+page.svelte
@@ -10,10 +10,10 @@
 	const eventName = "Karlsruhe"; // This should be the name of your event WITHOUT "Daydream" at the start
 	const eventLocation = "Karlsruhe";
 	const eventAddress = "TBA"; // Leave this empty if you don't want an address
-	const signupLink = "https://forms.hackclub.com/daydream-rsvp?event=rec6mTPjyxSSqtJab"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
+	const signupLink = "https://forms.hackclub.com/daydream-sign-up?event=rec6mTPjyxSSqtJab"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
 	// These two are optional-- leave them empty if you don't have anything!
-	const directionsURL = "TBA"
-	const contactLink = "TBA"
+	const directionsURL = ""
+	const contactLink = ""
 	
 	// Sponsors Configuration - disable this if you don't have any sponsors to display!
 	const sponsorsEnabled = false; // Set to false to hide the entire sponsors section
