a1a5a578d819c54d0262b8d332080366aa3bd223
Meet-Zone <meetzone@example.com>
2025-08-22 20:56:34 +0300

    Fix logo and date positioning to resolve overlap issue

--- src/routes/giza/+page.svelte
+++ src/routes/giza/+page.svelte
@@ -808,9 +808,9 @@ Mumbai`.split("\n")
 			<div class="h-12"></div> 
 			<!-- space for the ship -->
 			<h2
-			class="text-xl font-serif bg-gradient-to-b from-[#487DAB] to-[#3F709A] bg-clip-text text-transparent absolute left-1/2 max-sm:translate-y-4 max-sm:mb-0 max-md:-mb-8 md:left-[calc(50%+4rem)] -translate-x-1/2 bottom-8 italic w-max md:text-lg max-sm:text-lg"
+			class="text-xl font-serif bg-gradient-to-b from-[#487DAB] to-[#3F709A] bg-clip-text text-transparent absolute left-1/2 max-sm:translate-y-4 max-sm:mb-0 max-md:-mb-8 md:left-[calc(50%+4rem)] -translate-x-1/2 bottom-20 italic w-max md:text-lg max-sm:text-lg"
 			>
-				September 27th & 28th, 2025
+				September 27th, 2025
 			</h2>
 			<img src="/giza/DaydreamGizaLogo.svg" alt="Daydream Giza" class="h-40 mb-6 w-auto object-contain max-w-full px-4" />
 			<a href="https://hackclub.com" class="absolute top-0 -right-6 max-sm:right-0 max-sm:scale-80 animate-hover ![animation-delay:0.9s] ![--hover:-0.2rem]">
