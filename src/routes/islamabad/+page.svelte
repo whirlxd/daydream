cb797c3bf5ac752e86cb1474c9c96d6dccc89a9a
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2025-08-26 17:16:51 +0500

    Add islamabad event page

--- src/routes/islamabad/+page.svelte
+++ src/routes/islamabad/+page.svelte
@@ -7,16 +7,16 @@
 	 */
 
 	// Configuration - Put your information here!
-	const eventName = "Example"; // This should be the name of your event WITHOUT "Daydream" at the start
-	const eventLocation = "Example City";
-	const eventAddress = "1600 Pennsylvania Avenue, Washington, DC 20500"; // Leave this empty if you don't want an address
-	const signupLink = "https://forms.hackclub.com/daydream-sign-up"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
+	const eventName = "Islamabad"; // This should be the name of your event WITHOUT "Daydream" at the start
+	const eventLocation = "Islamabad";
+	const eventAddress = ""; // Leave this empty if you don't want an address
+	const signupLink = "https://forms.hackclub.com/daydream-sign-up?event=recQ1nPdqJzmF5MMi"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
 	// These two are optional-- leave them empty if you don't have anything!
-	const directionsURL = "https://www.google.com/maps/search/1600+pennsylvania+avenue+washington+dc/"
-	const contactLink = "mailto:example@daydream.hackclub.com"
+	const directionsURL = ""
+	const contactLink = "mailto:daydreamisb@gmail.com"
 	
 	// Sponsors Configuration - disable this if you don't have any sponsors to display!
-	const sponsorsEnabled = true; // Set to false to hide the entire sponsors section
+	const sponsorsEnabled = false; // Set to false to hide the entire sponsors section
 	const sponsors = [
 		{ image: "/example/logo1.png", name: "Sponsor 1", url: "https://example1.com" },
 		{ image: "/example/logo2.png", name: "Sponsor 2", url: "https://example2.com" },
@@ -34,22 +34,19 @@
 			items: [
 				{ event: "Doors open", time: "11:00 AM" },
 				{ event: "Opening ceremony", time: "12:00 PM" },
-				{ event: "Lunch", time: "12:30 PM" },
-				{ event: "Start working on your project!", time: "1:00 PM" },
-				{ event: "Workshop 1", time: "2:00 PM" },
+				{ event: "Start working on your project!", time: "12:30 PM" },
+				{ event: "Workshop 1", time: "1:00 PM" },
 				{ event: "Activity 1", time: "4:00 PM" },
-				{ event: "Workshop 2", time: "4:00 PM" },
-				{ event: "Dinner", time: "6:00 PM" },
-				{ event: "Lightning talks", time: "8:00 PM" },
-				{ event: "Midnight surprise", time: "12:00 AM" }
+				{ event: "Dinner", time: "7:00 PM" },
+				{ event: "Day end", time: "10:00 PM" }
 			]
 		},
 		{
 			title: "Sunday, September 28th",
 			items: [
-				{ event: "Breakfast", time: "8:00 AM" },
-				{ event: "Demos!", time: "10:30 AM" },
-				{ event: "Closing ceremony", time: "12:00 PM" }
+				{ event: "Doors open", time: "9:00 AM" },
+				{ event: "Demos!", time: "5:30 PM" },
+				{ event: "Closing ceremony", time: "7:00 PM" }
 			]
 		}
 	];
