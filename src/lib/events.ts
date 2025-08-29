8e0050068f649df7f1b619ce7f85fbe7cc80fd5e
Gus Ruben <gusruben@protonmail.com>
2025-08-29 12:15:54 -0400

    Hide API key error

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -52,6 +52,11 @@ if (AIRTABLE_API_KEY && AIRTABLE_BASE_ID) {
 }
 
 export async function loadAndGeocodeEvents(): Promise<EventLocation[]> {
+	// do nothing if the API keys aren't set
+	if (!AIRTABLE_API_KEY || !AIRTABLE_BASE_ID || !GEOCODER_API_KEY) {
+		return [];
+	}
+	
 	if (cachedEvents) {
 		return cachedEvents;
 	}
