40c3cbc4ba6844adc931a0cee794dbbf9e623ad6
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2025-08-30 01:24:42 +0500

    Undo that new button sir dawood asked for 😭

--- src/routes/lahore/+page.svelte
+++ src/routes/lahore/+page.svelte
@@ -829,7 +829,6 @@ Mumbai`.split("\n")
 				Organized by Teenagers in {@html eventLocation.replaceAll(" ", "&nbsp;")}
 			</h4>
 		</div>
-		<a href="{signupLink}">Sign Up Now</a>
 		<ParticipantSignUp {signupLink} {eventName} />
 	</div>
 
