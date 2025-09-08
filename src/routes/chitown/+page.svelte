92918d50c22e95f070c44fd5191748c8882b664a
wessleyn <wessleynyakaz@gmail.com>
2025-09-08 23:02:48 +0200

    chore: added  chitungwiza signupLink

--- src/routes/chitown/+page.svelte
+++ src/routes/chitown/+page.svelte
@@ -10,7 +10,7 @@
 	const eventName = "Chitungwiza"; // This should be the name of your event WITHOUT "Daydream" at the start
 	const eventLocation = "Chitungwiza";
 	const eventAddress = ""; // Leave this empty if you don't want an address
-	const signupLink = "https://forms.hackclub.com/daydream-sign-up"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
+	const signupLink = "https://forms.hackclub.com/daydream-sign-up?event=recMLlwG4xNrWCcfm"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
 	// These two are optional-- leave them empty if you don't have anything!
 	const directionsURL = ""
 	const contactLink = "" // "mailto:example@daydream.hackclub.com"
