81c596e37165bbca1299a49f946ec6ec3454016f
ssno0 <grahmcrackers96@hotmail.com>
2025-09-11 07:05:05 +1000

    chat i accidently put QUT instead of brisbane, i was confused

--- src/routes/brisbane/+page.svelte
+++ src/routes/brisbane/+page.svelte
@@ -8,8 +8,8 @@
 
 	// Configuration - Put your information here!
 	const eventName = "Brisbane"; // This should be the name of your event WITHOUT "Daydream" at the start
-	const eventLocation = "QUT Gardens Point";
-	const eventAddress = "2 George Street, Brisbane, QLD 4000, Australia"; // Leave this empty if you don't want an address
+	const eventLocation = "Brisbane";
+	const eventAddress = "2 George Street, Brisbane, QLD 4000, Australia; // Leave this empty if you don't want an address
 	const signupLink = "https://forms.hackclub.com/daydream-sign-up?event=recofLAm7EG3zrCID"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
 	// These two are optional-- leave them empty if you don't have anything!
 	const directionsURL = "https://maps.app.goo.gl/PkM4oPkMAcmExE7c7"
