7c665f68202f4c10c5ce490e2225f6be7e088acb
luke89joseph <luke87joseph@gmail.com>
2025-09-19 18:42:29 -0400

    Add venue location

--- src/routes/nyc/+page.svelte
+++ src/routes/nyc/+page.svelte
@@ -9,7 +9,7 @@
 	// Configuration - Put your information here!
 	const eventName = "NYC"; // This should be the name of your event WITHOUT "Daydream" at the start
 	const eventLocation = "New York City";
-	const eventAddress = ""; // Leave this empty if you don't want an address
+	const eventAddress = "Gumbo NYC @ 53 Bridge St. Suite 301"; // Leave this empty if you don't want an address
 	const signupLink = "https://forms.hackclub.com/daydream-sign-up?event=recScab6KjhWs1rGe"; // Get your custom sign up link from this page: https://airtable.com/apppg7RHZv6feM66l/shr4kFqURo8fMIRie
 	// These two are optional-- leave them empty if you don't have anything!
 	const directionsURL = ""
