9dac55a2742342d4b15ae1b43ae6b0b6dcb3ba8f
Issac Liu <issac@matchatea.dev>
2025-09-19 01:27:58 -0400

    use local version of makerspace logo

--- src/routes/nyc/+page.svelte
+++ src/routes/nyc/+page.svelte
@@ -20,7 +20,7 @@
 	const sponsors = [
 		{ image: "/nyc/medal.png", name: "Medal", url: "https://medal.tv" },
 		{ image: "/nyc/jukebox-logo.svg", name: "Jukebox", url: "https://jukeboxprint.com" },
-		{ image: "https://static.wixstatic.com/media/2d4ffd_deb3c3847e6649f5923c78a8dd21aa60~mv2.png/v1/crop/x_54,y_0,w_1392,h_1503/fill/w_556,h_596,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/0Kc-k22A.png", name: "Makerspace NYC", url: "https://www.makerspace.nyc" },
+		{ image: "/nyc/makerspace.png", name: "Makerspace NYC", url: "https://www.makerspace.nyc" },
 	];
 	const specialThanks = {
 		kirk : { image: "/nyc/kirk.jpg", name: "Kirk Musngi", title: "Senior Community Manager", affiliation: "Unity Technologies", for: "helping us get our venue!" },
